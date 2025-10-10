81ef44dc2b91777f3ec3e4268f652397598b58f5
Deedat116116 <95043077+Deedat116116@users.noreply.github.com>
2025-10-10 15:05:02 +0000

    Set up the person class not complete!

---
+++ Person.py
@@ -0,0 +1,28 @@
+
+
+class Person:
+    def __init__(money = 100):
+        self.cards = []
+        self.cards_value = 0
+        self.money = money
+
+    def get_cards():
+        return self.cards
+
+    def get_value():
+        return self.cards_value
+        
+    def get_money():
+        pass
+
+    def add_card(card):
+        self.cards.append(card)
+        self.cards_value += card.get_value()
+
+    def add_money(amount):
+        self.add_money +=
+
+    def reset_cards():
+        pass
+
+
