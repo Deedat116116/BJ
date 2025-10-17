44fa58730916a8c1e44cfb1229a8d7064ca66b76
Deedat <Deedat116116@mcpsva.org>
2025-10-17 19:13:24 -0400

    Chnaged This it is more clear

--- Person.py
+++ Person.py
@@ -1,32 +1,32 @@
 
 
 class Person:
-    def __init__(money = 100):
+    def __init__(self, money = 100):
         self.cards = []
         self.cards_value = 0
         self.money = money
 
-    def get_cards():
+    def get_cards(self):
         return self.cards
 
-    def get_value():
+    def get_value(self):
         return self.cards_value
         
-    def get_money():
+    def get_money(self):
         pass
 
-    def add_card(card):
+    def add_card(self,card):
         self.cards.append(card)
         self.cards_value += card.get_value()
 
-    def add_value(value):
+    def add_value(self,value):
         self.cards_value += value
 
 
-    def add_money(amount):
+    def add_money(amount, self):
         self.add_money += self.money
 
-    def reset_cards():
+    def reset_cards(cards_value):
         return cards_value
 
 
